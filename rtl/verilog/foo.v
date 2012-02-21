d5a9685224510deee7874712fbed1d7fa8956852
Phil Tracton <ptracton@gmail.com>
2012-02-20 21:47:39 -0800

    Putting material on branches

---
+++ rtl/verilog/foo.v
@@ -0,0 +1,19 @@
+//                              -*- Mode: Verilog -*-
+// Filename        : foo.v
+// Description     : GIT TESTING
+// Author          : Phil Tracton
+// Created On      : Mon Feb 20 21:46:47 2012
+// Last Modified By: .
+// Last Modified On: .
+// Update Count    : 0
+// Status          : Unknown, Use with caution!
+
+
+module foo(input wire clk,
+	   input wire rst,
+	   input wire rx,
+	   output reg tx
+	   );
+
+
+endmodule // foo
